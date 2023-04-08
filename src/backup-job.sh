9e44442bf1d75f3988e12788c1af287c25f5683e
Vedant_Tarale <vedanttarale.211ec257@nitk.edu.in>
2023-04-08 20:33:29 +0530

    added backup deamon

---
+++ src/backup-job.sh
@@ -0,0 +1,2 @@
+!#/bin/bash
+mysqldump -u root -p 12345678  > /backups/backup.sql
--- src/config/application.rb
+++ src/config/application.rb
@@ -18,5 +18,6 @@ module EmpMan
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << 'Rails'
   end
 end
